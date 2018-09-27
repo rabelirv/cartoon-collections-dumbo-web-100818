442c4ee2f5f7f711618a12d30de3c12ff56b7b9b
Irving E Rabel <rabel606@gmail.com>
2018-09-27 23:51:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,6 +22,7 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 if array.include?(cheese_types)
-return "cheddar"
+array.each {|element| element == cheese_types
+return element}
 end
 end
