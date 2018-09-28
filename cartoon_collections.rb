1b7ddb43096a6bde8f22e321b9b4739fbc1ce967
Irving E Rabel <rabel606@gmail.com>
2018-09-28 01:26:39 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+  counter=1 
+  dwarves.each {|dwarf| puts"#{counter}. #{dwarf}"
+  counter += 1 }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  #
+  new_elements = planeteer_calls.collect{|element|element + "!"}
+  new_elements.collect {|element| element.capitalize!}
+  new_elements
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
+  calls.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+array.find do |element| 
+element == "cheddar"||element =="gouda"||element=="camembert"
+end 
 end
