c94e9a7f6d241d946660f4ddf99e03c7ad14aea8
Irving E Rabel <rabel606@gmail.com>
2018-09-28 01:19:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-array.include? do |element| 
+array.find do |element| 
 element == "cheddar"||element =="gouda"||element=="camembert"
 end 
 end
